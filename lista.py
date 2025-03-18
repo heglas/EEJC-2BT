5c87d0276bde3d688c52793e0208703b114042ce
Heglas Oliveira <heglas@gmail.com>
2025-03-18 10:28:45 -0300

    Programas exemplo de lista 17/03/2025

---
+++ lista.py
@@ -0,0 +1,7 @@
+primeira_lista = []
+print (primeira_lista)
+print (type(primeira_lista))
+
+segunda_lista = list()
+print(segunda_lista)
+print(type(segunda_lista))
---
+++ melhorsala.py
@@ -0,0 +1,2 @@
+lista = ["2BT","2AT","2C"]
+print("o melhor segundo é o "+ lista[0])
