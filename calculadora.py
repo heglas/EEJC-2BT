9c2ffe925147267609d402801c9bc6acafbfcaf3
Heglas Oliveira <heglas@gmail.com>
2025-03-21 15:01:30 -0300

    Update semanal - 6ª Semana

--- calculadora.py
+++ calculadora.py
@@ -2,14 +2,15 @@ op=input("Entre com A para Soma, S para subtraçao, D para divisão e M para Mul
 if (op!="A" and op!="S" and op!="D" and op!="M"):
     print("Erro: operaçao não reconhecida")
 else:
-    opr1 = int(input("entre com o primeiro valor:"))
+    opr1=int(input("entre com o primeiro valor:"))
     opr2=int(input("entre com o segundo valor:"))
-    if(op=="SOMA"):
-        result = opr1 + opr2
-    elif(op=="SUBTRAÇÃO"):
-        result = opr1 - opr2
-    elif(op=="DIVISÃO"):
-         result = opr1 / opr2
-    elif(op=="MULTIPLICAÇÃO"):
-        result = opr1 * opr2
-    print (result,"\n")
+    if (op=="A"):
+        r=opr1+opr2
+    elif (op=="S"):
+        r=opr1-opr2
+    elif (op=="D"):
+        r=opr1/opr2  
+    elif (op=="M"):
+        r=opr1*opr2  
+    print ("o resultado da operação é:" + str(r))
+    
